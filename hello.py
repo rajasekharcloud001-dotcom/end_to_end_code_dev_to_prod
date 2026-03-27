aba14b04d46f88ebab66cdb720d21c0d5ff1c255
Rajasekhar Reddy <rajasekhar.cloud001@gmail.com>
2026-03-28 04:14:55 +0530

    Add hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+import os
+def say_hello():
+    env = os.getenv("ENV_NAME","local")
+    project = os.getenv("GCP_PROJECT","unknow-project")
+
+    message = f"hello from {env.upper()} enviroment"
+    print(message)
+    print(f"gcp project :{project}")
+if __name__=="__main__":
+    say_hello()
+
